ee195f1ae54b74401dc2e9c4cf5209cb61425a8c
Olivier <coq.olivier36@gmail.com>
2025-12-24 16:24:36 -0500

    Father, forgive me

--- src/agent.ts
+++ src/agent.ts
@@ -1,35 +1,52 @@
 import type { AIMessage } from '../types'
-import { addMessages, getMessages, saveToolResponse } from './memory'
 import { runLLM } from './llm'
-import { showLoader, logMessage } from './ui'
+import { z } from 'zod'
 import { runTool } from './toolRunner'
+import { addMessages, getMessages, saveToolResponse } from './memory'
+import { logMessage, showLoader } from './ui'
 
 export const runAgent = async ({
+  turns = 10,
   userMessage,
-  tools,
+  tools = [],
 }: {
+  turns?: number
   userMessage: string
-  tools: any[]
+  tools?: { name: string; parameters: z.AnyZodObject }[]
 }) => {
-  await addMessages([{ role: 'user', content: userMessage }])
+  await addMessages([
+    {
+      role: 'user',
+      content: userMessage,
+    },
+  ])
 
-  const loader = showLoader('🤔')
-  const history = await getMessages()
+  const loader = showLoader('Thinking...')
 
-  const response = await runLLM({ messages: history, tools })
-  await addMessages([response])
+  while (true) {
+    const history = await getMessages()
+    const response = await runLLM({
+      messages: history,
+      tools,
+    })
 
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0]
+    await addMessages([response])
 
-    loader.update(`executing: ${toolCall.function.name}`)
+    logMessage(response)
 
-    const toolResponse = await runTool(toolCall, userMessage)
-    await saveToolResponse(toolCall.id, toolResponse)
-    loader.update(`done: ${toolCall.function.name}`)
+    if (response.content) {
+      loader.stop()
+      return getMessages()
+    }
+
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0]
+      loader.update(`executing: ${toolCall.function.name}`)
+
+      const toolResponse = await runTool(toolCall, userMessage)
+      await saveToolResponse(toolCall.id, toolResponse)
+
+      loader.update(`executed: ${toolCall.function.name}`)
+    }
   }
-
-  logMessage(response)
-  loader.stop()
-  return getMessages()
 }
