14a987dcfad4c3e86ed43359047c218200a1e3b0
Olivier <coq.olivier36@gmail.com>
2025-12-24 16:50:06 -0500

    Moon

--- src/toolRunner.ts
+++ src/toolRunner.ts
@@ -1,6 +1,6 @@
 import type OpenAI from 'openai'
 
-const getWeather = () => `hot, 90deg`
+const getWeather = (input: any) => `hot, 90deg`
 
 export const runTool = async (
   toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall,
